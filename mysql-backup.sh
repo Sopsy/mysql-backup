dc54397a0b0fb4bf55e072e36e52fbd76d2d79af
Aleksi Kinnunen <721951+Sopsy@users.noreply.github.com>
2019-05-02 15:21:49 +0200

    Increment version number

--- mysql-backup.sh
+++ mysql-backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Shell script to backup MySQL databases
-# Version: 1.7
+# Version: 1.8
 # Author: Aleksi "Sopsy" Kinnunen
 # URL: https://github.com/Sopsy/mysql-backup
 # License: AGPLv3 or later
